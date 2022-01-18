de0667f423650b19574db47def1bf3c8c20b4db5
HarishKumarMeena <60287607+HarishKumarMeena@users.noreply.github.com>
2022-01-18 21:31:41 +0530

    Update Task_1_C.cpp
        // sum of natural numbers = n*(n+1)/2
        // finding the roots 
       // return the lowest root

--- Task1/Task_1_C.cpp
+++ Task1/Task_1_C.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 double rows_with_boxes(double boxes)
 {
+    // sum of natural num = n*(n+1)/2
+    // finding the roots 
     double rows_root_one = (-1 + sqrt((1 + 4 * 2 * boxes))) / 2;
     double rows_root_two = (-1 - sqrt((1 + 4 * 2 * boxes))) / 2;
 
+    // returning the lower root
     if (rows_root_one >= rows_root_two)
         return rows_root_one;
 
