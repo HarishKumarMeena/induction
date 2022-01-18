4cf0505dc4081ba5c962dcf26d442f08c25c2bf8
HarishKumarMeena <60287607+HarishKumarMeena@users.noreply.github.com>
2022-01-18 21:23:45 +0530

    Update Task_1_A.cpp
    // making the array of toffies price
    //sorting the array O(n)
    // reducing the price of toffies from total rupees

--- Task1/Task_1_A.cpp
+++ Task1/Task_1_A.cpp
@@ -11,9 +11,11 @@ int main()
 
     int count = 0;
 
+    // making the array of toffie price
     for (int i = 0; i < toffies; i++)
         cin >> toffie_price[i];
-
+    
+    //sorting the array O(n)
     for (int j = 0; j < toffies; j++)
     {
         for (int k = 0; k < toffies - 1; k++)
@@ -26,7 +28,8 @@ int main()
             }
         }
     }
-
+    
+    // reducing the price of toffies from total rupees 
     for (int l = 0; l < toffies; l++)
     {
 
